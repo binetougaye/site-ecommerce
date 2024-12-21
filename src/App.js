fec054617840ec3f8016ee2bb888ad70de664e06
binetougaye <fabigaye3@gmail.com>
2024-12-21 14:27:22 +0000

    about us & team

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import About from './Components/About_Us/About.jsx';
+import Team from './Components/Team/Team.jsx';
+import Testimonials from './Components/Testimonials/Testimonials.jsx';
 import NavBar from './NavBar/NavBar.jsx';
 
 function App() {
   return (
-    <div className="App">
-    <NavBar/>
+    <div>
+      <NavBar />
+      <div className='px-16'>
+        <About />
+        <Team />
+        <Testimonials />
+      </div>
+
     </div>
   );
 }
